eaf4dd55c195b5ddb4bdfc3aaf54a7e249dc0d65
Kristijan Bucevski <kristijan.bucevski@bridgeportschools.net>
2017-01-26 20:33:02 +0000

    adding_offset_loop

--- geometrical_shape/main.js
+++ geometrical_shape/main.js
@@ -1,17 +1,24 @@
 var NUM_CIRCLES = 12;
 var circleDiametar;
+var circleRadius;
 
 function setup() {
     createCanvas(480, 600);
     circleDiametar = width/ NUM_CIRCLES;
+    circleRadius = circleDiametar/ 2;
 }
 function draw() {
     var x = 0;
     var y = 0;
+    var isShifted = false;
     
     while(y <= height){
-        
-    x = 0;
+        if(isShifted){
+            x = circleRadius;
+        }
+        else{
+            x = 0;
+        }
     while(x <= width){
         
         ellipse(x, y, circleDiametar,circleDiametar);
@@ -19,7 +26,8 @@ function draw() {
         x= x + circleDiametar;
     }
     
-    y= y + circleDiametar;
+    y= y + circleRadius;
+    isShifted = !isShifted;
     }
     
      
